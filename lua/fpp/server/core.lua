9a79cdbd7236e2446041480955f57b57521e72d7
FPtje <fpeijnenburg@gmail.com>
2015-08-26 20:33:24 +0200

    PlayerSpawnRagdoll blacklisted models #136

--- lua/fpp/server/core.lua
+++ lua/fpp/server/core.lua
@@ -41,6 +41,7 @@ local function propSpawn(ply, model)
 end
 hook.Add("PlayerSpawnProp", "FPP_SpawnProp", propSpawn)
 hook.Add("PlayerSpawnEffect", "FPP_SpawnEffect", propSpawn)
+hook.Add("PlayerSpawnRagdoll", "FPP_SpawnEffect", propSpawn)
 
 --------------------------------------------------------------------------------------
 --Helper function to get a setting from a player
