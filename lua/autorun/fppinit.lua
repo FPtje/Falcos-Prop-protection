ebb2b9c235770a9805f4df2876e38f432d037992
FPtje <fpeijnenburg@gmail.com>
2015-10-02 16:46:24 +0200

    Converted tabs to spaces + fixed lint warnings

--- lua/autorun/fppinit.lua
+++ lua/autorun/fppinit.lua
@@ -1,41 +1,41 @@
 if SERVER then
-	AddCSLuaFile()
+    AddCSLuaFile()
 
-	include("config/mysql.lua")
-	include("libraries/mysqlite/mysqlite.lua")
+    include("config/mysql.lua")
+    include("libraries/mysqlite/mysqlite.lua")
 
-	include("libraries/sh_cami.lua")
-	include("fpp/sh_cppi.lua")
-	include("fpp/sh_settings.lua")
+    include("libraries/sh_cami.lua")
+    include("fpp/sh_cppi.lua")
+    include("fpp/sh_settings.lua")
 
-	include("fpp/server/defaultblockedmodels.lua")
-	include("fpp/server/settings.lua")
-	include("fpp/server/core.lua")
-	include("fpp/server/antispam.lua")
-	include("fpp/server/ownability.lua")
+    include("fpp/server/defaultblockedmodels.lua")
+    include("fpp/server/settings.lua")
+    include("fpp/server/core.lua")
+    include("fpp/server/antispam.lua")
+    include("fpp/server/ownability.lua")
 
-	AddCSLuaFile("fpp/client/menu.lua")
-	AddCSLuaFile("fpp/client/hud.lua")
-	AddCSLuaFile("fpp/client/buddies.lua")
-	AddCSLuaFile("fpp/client/ownability.lua")
+    AddCSLuaFile("fpp/client/menu.lua")
+    AddCSLuaFile("fpp/client/hud.lua")
+    AddCSLuaFile("fpp/client/buddies.lua")
+    AddCSLuaFile("fpp/client/ownability.lua")
 
-	AddCSLuaFile("libraries/sh_cami.lua")
-	AddCSLuaFile("fpp/sh_cppi.lua")
-	AddCSLuaFile("fpp/sh_settings.lua")
+    AddCSLuaFile("libraries/sh_cami.lua")
+    AddCSLuaFile("fpp/sh_cppi.lua")
+    AddCSLuaFile("fpp/sh_settings.lua")
 
-	if FPP_MySQLConfig and FPP_MySQLConfig.EnableMySQL then
-		hook.Add("DatabaseInitialized", "FPP Init", FPP.Init)
-		MySQLite.connectToMySQL(FPP_MySQLConfig.Host, FPP_MySQLConfig.Username, FPP_MySQLConfig.Password, FPP_MySQLConfig.Database_name, FPP_MySQLConfig.Database_port)
-	else
-		FPP.Init()
-	end
+    if FPP_MySQLConfig and FPP_MySQLConfig.EnableMySQL then
+        hook.Add("DatabaseInitialized", "FPP Init", FPP.Init)
+        MySQLite.connectToMySQL(FPP_MySQLConfig.Host, FPP_MySQLConfig.Username, FPP_MySQLConfig.Password, FPP_MySQLConfig.Database_name, FPP_MySQLConfig.Database_port)
+    else
+        FPP.Init()
+    end
 elseif CLIENT then
-	include("fpp/client/menu.lua")
-	include("fpp/client/hud.lua")
-	include("fpp/client/buddies.lua")
-	include("fpp/client/ownability.lua")
+    include("fpp/client/menu.lua")
+    include("fpp/client/hud.lua")
+    include("fpp/client/buddies.lua")
+    include("fpp/client/ownability.lua")
 
-	include("libraries/sh_cami.lua")
-	include("fpp/sh_cppi.lua")
-	include("fpp/sh_settings.lua")
+    include("libraries/sh_cami.lua")
+    include("fpp/sh_cppi.lua")
+    include("fpp/sh_settings.lua")
 end
--- lua/config/mysql.lua
+++ lua/config/mysql.lua
@@ -6,15 +6,15 @@ In this file you can find a manual for MySQL configuration and the MySQL config 
 ]]
 
 
-FPP_MySQLConfig.EnableMySQL = false -- Set to true if you want to use an external MySQL database, false if you want to use the built in SQLite database (garrysmod/sv.db) of Garry's mod.
-FPP_MySQLConfig.Host = "127.0.0.1" -- This is the IP address of the MySQL host. Make sure the IP address is correct and in quotation marks (" ")
-FPP_MySQLConfig.Username = "user" -- This is the username to log in on the MySQL server.
-								-- contact the owner of the server about the username and password. Make sure it's in quotation marks! (" ")
-FPP_MySQLConfig.Password = "password" -- This is the Password to log in on the MySQL server,
-									-- Everyone who has access to FTP on the server can read this password.
-									-- Make sure you know who to trust. Make sure it's in quotation marks (" ")
-FPP_MySQLConfig.Database_name = "FPP" -- This is the name of the Database on the MySQL server. Contact the MySQL server host to find out what this is
-FPP_MySQLConfig.Database_port = 3306 -- This is the port of the MySQL server. Again, contact the MySQL server host if you don't know this.
+FPP_MySQLConfig.EnableMySQL = false     -- Set to true if you want to use an external MySQL database, false if you want to use the built in SQLite database (garrysmod/sv.db) of Garry's mod.
+FPP_MySQLConfig.Host = "127.0.0.1"      -- This is the IP address of the MySQL host. Make sure the IP address is correct and in quotation marks (" ")
+FPP_MySQLConfig.Username = "user"       -- This is the username to log in on the MySQL server.
+                                        -- contact the owner of the server about the username and password. Make sure it's in quotation marks! (" ")
+FPP_MySQLConfig.Password = "password"   -- This is the Password to log in on the MySQL server,
+                                        -- Everyone who has access to FTP on the server can read this password.
+                                        -- Make sure you know who to trust. Make sure it's in quotation marks (" ")
+FPP_MySQLConfig.Database_name = "FPP"   -- This is the name of the Database on the MySQL server. Contact the MySQL server host to find out what this is
+FPP_MySQLConfig.Database_port = 3306    -- This is the port of the MySQL server. Again, contact the MySQL server host if you don't know this.
 
 
 --[[
@@ -26,17 +26,17 @@ http://www.facepunch.com/showthread.php?t=933647
 Unpack it
 
 on the SERVER:
-	- Put libmySQL.dll in the same folder as HL2.exe OR srcds.exe (e.g srcds\orangebox OR steamapps\username\garrysmod)
-	- Put gmsv_mysqloo.dll in garrysmod\lua\includes\modules\. If any of those folders don't exist, create them.
-		You can also copy and paste the lua folder from the download to garrysmod/
-	- Make sure the configurations in this file (_MySQL.lua) are set up correctly
-	- TIP: Make this file read-only so it doesn't get changed in an SVN update!
-		But if you get problems with the MySQL database, remove this file, update from SVN and set it up again, things might change in the SVN!
+    - Put libmySQL.dll in the same folder as HL2.exe OR srcds.exe (e.g srcds\orangebox OR steamapps\username\garrysmod)
+    - Put gmsv_mysqloo.dll in garrysmod\lua\includes\modules\. If any of those folders don't exist, create them.
+        You can also copy and paste the lua folder from the download to garrysmod/
+    - Make sure the configurations in this file (_MySQL.lua) are set up correctly
+    - TIP: Make this file read-only so it doesn't get changed in an SVN update!
+        But if you get problems with the MySQL database, remove this file, update from SVN and set it up again, things might change in the SVN!
 
 
 
 WHAT TO DO IF YOU CAN'T GET IT TO WORK!
-	- There are always errors on the server, try if you can see those (with HLDS)
-	- make sure the settings in this file (_MySQL.lua) are correct
-	- make sure the MySQL server is accessible from the outside
+    - There are always errors on the server, try if you can see those (with HLDS)
+    - make sure the settings in this file (_MySQL.lua) are correct
+    - make sure the MySQL server is accessible from the outside
 ]]
